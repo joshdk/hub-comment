aac98e9ea7b20ba82bf4a64823fd2e92d3802634
Josh Komoroske <jdkomo@gmail.com>
2018-10-09 20:08:04 -0700

    CircleCI build configuration (#2)

---
+++ main.go
@@ -0,0 +1,9 @@
+// Copyright Josh Komoroske. All rights reserved.
+// Use of this source code is governed by the MIT license,
+// a copy of which can be found in the LICENSE.txt file.
+
+package main
+
+var version = "development"
+
+func main() {}
