6e959f81337ceaadf7aa639d83242711d4036373
Josh Komoroske <jdkomo@gmail.com>
2018-10-11 00:03:49 -0700

    Look up the latest pull request comment (#5)

---
+++ hub/comment.go
@@ -0,0 +1,48 @@
+// Copyright Josh Komoroske. All rights reserved.
+// Use of this source code is governed by the MIT license,
+// a copy of which can be found in the LICENSE.txt file.
+
+package hub
+
+import (
+	"context"
+
+	"github.com/google/go-github/github"
+)
+
+// GetComments fetches all comments for the given pull request number.
+func GetComments(ctx context.Context, client *github.Client, owner string, repo string, number int) ([]*github.IssueComment, error) {
+	comments, _, err := client.Issues.ListComments(ctx, owner, repo, number, nil)
+	if err != nil {
+		return nil, err
+	}
+	return comments, nil
+}
+
+// FilterComments selects the most recently updated comment, that was authored
+// by the current user, if any exist.
+func FilterComments(comments []*github.IssueComment, name string) (int64, bool) {
+	var best *github.IssueComment
+	for _, comment := range comments {
+		user := comment.GetUser()
+		if user == nil {
+			continue
+		}
+
+		// Reject any comment that was not authored by the current user.
+		author := user.GetLogin()
+		if author != name {
+			continue
+		}
+
+		// Keep any comment that has been more recently updated.
+		if best == nil || comment.GetUpdatedAt().After(best.GetUpdatedAt()) {
+			best = comment
+		}
+	}
+
+	if best != nil {
+		return *best.ID, true
+	}
+	return 0, false
+}
