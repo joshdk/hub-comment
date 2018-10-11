9fb07a84db31a0db0adca88c3e215053847a15d6
Josh Komoroske <jdkomo@gmail.com>
2018-10-10 21:46:02 -0700

    Parse CircleCI pull request link from env (#4)

--- main.go
+++ main.go
@@ -11,9 +11,19 @@ import (
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
+	"github.com/joshdk/hub-comment/hub"
 )
 
-const tokenEnvVar = "GITHUB_TOKEN"
+const (
+	// githubTokenEnvVar is the name of the environment variable which holds
+	// the token used for authenticating against the GitHub API.
+	githubTokenEnvVar = "GITHUB_TOKEN"
+
+	// pullRequestLinkEnvVar is the name of the environment variable which
+	// holds a link to the current pull request. Injected automatically by
+	// CircleCI.
+	pullRequestLinkEnvVar = "CIRCLE_PULL_REQUEST"
+)
 
 var version = "development"
 
@@ -25,9 +35,23 @@ func main() {
 }
 
 func mainCmd() error {
-	token, found := os.LookupEnv(tokenEnvVar)
+	token, found := os.LookupEnv(githubTokenEnvVar)
 	if !found {
 		return fmt.Errorf("no GITHUB_TOKEN set in environment")
+	}
+
+	// If no CIRCLE_PULL_REQUEST is set, print an error and return immediately
+	// but do not fail. This environment variable will not be set on non-pr
+	// branches, or if a build is started before a pr is opened.
+	reference, found := os.LookupEnv(pullRequestLinkEnvVar)
+	if !found {
+		fmt.Fprintln(os.Stderr,"hub-comment: no CIRCLE_PULL_REQUEST set in environment")
+		return nil
+	}
+
+	owner, repo, number, found := hub.SplitPullRequestReference(reference)
+	if !found {
+		return fmt.Errorf("malformed pull request link")
 	}
 
 	var (
@@ -42,6 +66,7 @@ func mainCmd() error {
 	}
 
 	fmt.Printf("User is %s (%s)\n", self.GetName(), self.GetLogin())
+	fmt.Printf("Pull is %s/%s #%d\n", owner, repo, number)
 
 	return nil
 }
