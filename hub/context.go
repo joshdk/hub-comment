db61df72421a0cb0dbc9549384c05679d4aab2f7
Josh Komoroske <jdkomo@gmail.com>
2018-10-17 00:58:06 -0700

    Trim template whitespace

--- hub/context.go
+++ hub/context.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"strings"
 	"text/template"
+	"unicode"
 )
 
 // Context represents a logical grouping of data for use with comment templates.
@@ -80,7 +81,7 @@ func NewContext(environ []string) Context {
 
 // NewTemplate is a helper for constructing a template object.
 func NewTemplate(body []byte) (*template.Template, error) {
-	return template.New("comment").Parse(string(body))
+	return template.New("comment").Parse(trim(string(body)))
 }
 
 // Execute applies the given context to the given template and returns the
@@ -90,5 +91,43 @@ func Execute(tpl *template.Template, ctx Context) (string, error) {
 	if err := tpl.Execute(&buf, ctx); err != nil {
 		return "", err
 	}
-	return buf.String(), nil
+	return trim(buf.String()), nil
+}
+
+// trim returns the given input string, with all trailing whitespace characters
+// and all leading blank lines removed. Leading whitespace on the first
+// non-blank line is kept intact. This behavior is useful, specifically, when
+// the first line of a comment is a 4-space-indented code block.
+func trim(raw string) string {
+	var (
+		firstSpace = 0
+		firstChar  = 0
+		lastChar   = len(raw) - 1
+	)
+
+	// Scan backwards starting from the end of the string. Find index of the
+	// first non-whitespace character.
+	for ; lastChar >= 0; lastChar-- {
+		if !unicode.IsSpace(rune(raw[lastChar])) {
+			break
+		}
+	}
+
+	// Scan forwards starting from the beginning of the string. Find index of
+	// the first non-whitespace character.
+	for ; firstChar < lastChar; firstChar++ {
+		if !unicode.IsSpace(rune(raw[firstChar])) {
+			break
+		}
+	}
+
+	// Scan backwards starting from the first non-whitespace character. Find
+	// index of the first newline character.
+	for firstSpace = firstChar - 1; firstSpace >= 0; firstSpace-- {
+		if raw[firstSpace] == '\n' || raw[firstSpace] == '\r' {
+			break
+		}
+	}
+
+	return raw[firstSpace+1 : lastChar+1]
 }
---
+++ hub/context_test.go
@@ -0,0 +1,99 @@
+package hub
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrim(t *testing.T) {
+	tests := []struct {
+		title    string
+		body     string
+		expected string
+	}{
+		{
+			title: "empty string",
+		},
+
+		{
+			title: "only spaces",
+			body:  "    ",
+		},
+		{
+			title: "only newlines",
+			body:  "\n\n\n\n",
+		},
+		{
+			title: "spaces with leading newlines",
+			body:  "\n\n\n\n    ",
+		},
+		{
+			title: "newlines with leading spaces",
+			body:  "    \n\n\n\n",
+		},
+		{
+			title:    "single line",
+			body:     "hello world",
+			expected: "hello world",
+		},
+		{
+			title:    "single line with leading spaces",
+			body:     "    hello world",
+			expected: "    hello world",
+		},
+		{
+			title:    "single line with trailing spaces",
+			body:     "hello world    ",
+			expected: "hello world",
+		},
+		{
+			title:    "single line with surrounding spaces",
+			body:     "    hello world    ",
+			expected: "    hello world",
+		},
+		{
+			title:    "single line with leading newlines",
+			body:     "\n\nhello world",
+			expected: "hello world",
+		},
+		{
+			title:    "single line with leading newlines and spaces",
+			body:     "    \n    \n    hello world",
+			expected: "    hello world",
+		},
+		{
+			title:    "single line with trailing newlines",
+			body:     "hello world\n\n",
+			expected: "hello world",
+		},
+		{
+			title:    "single line with trailing newlines and spaces",
+			body:     "hello world    \n    \n    ",
+			expected: "hello world",
+		},
+		{
+			title:    "single unicode",
+			body:     "🤖",
+			expected: "🤖",
+		},
+		{
+			title:    "multiple unicode",
+			body:     "    \n    \n    🤖    🤖    \n    \n    ",
+			expected: "    🤖    🤖",
+		},
+	}
+
+	for index, test := range tests {
+		name := fmt.Sprintf("%d %s", index+1, test.title)
+		t.Run(name, func(t *testing.T) {
+			actual := trim(test.body)
+
+			assert.False(t, strings.HasPrefix(actual, "\n"))
+			assert.False(t, strings.HasSuffix(actual, "\n"))
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
